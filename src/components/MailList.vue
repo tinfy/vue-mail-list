c4a3be8ac78a211d150765f02dd3ffaa680114e5
tinfy <tongjiebin@163.com>
2021-09-16 23:39:38 +0800

    修改通讯录界面

--- src/components/MailList.vue
+++ src/components/MailList.vue
@@ -4,6 +4,15 @@
       <el-input placeholder="请输入搜索联系人" v-model="queryParam" clearable class="query_input">
         <el-button slot="append" icon="el-icon-search"></el-button>
       </el-input>
+      <p>共有 {{contactNum}} 个联系人</p>
+      <div>
+        <el-radio v-model="radio" label="1">所有联系人</el-radio>
+        <el-radio v-model="radio" label="2">亲人</el-radio>
+        <el-radio v-model="radio" label="3">朋友</el-radio>
+        <el-radio v-model="radio" label="4">同学</el-radio>
+      </div>
+      <div>
+      </div>
     </el-card>
   </div>
 </template>
@@ -12,7 +21,9 @@
 export default {
   data () {
     return {
-      queryParam: ''
+      queryParam: '',
+      contactNum: 0,
+      radio: '1'
     }
   },
   methods: {
@@ -23,6 +34,6 @@ export default {
 
 <style lang="scss" scoped>
 .query_input {
-    width: 600px;
+  width: 600px;
 }
 </style>
